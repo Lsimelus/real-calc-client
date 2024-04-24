0ec90746cb83fcef73225a65a0677a69ad9bddbd
Lsimelus <lsimelus@gmail.com>
2024-04-24 15:10:47 -0400

    redux set up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Provider } from "../../node_modules/react-redux/dist/react-redux";
 import "./globals.css";
+import StoreProvider from "../lib/storeProvider"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <StoreProvider>
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
+    </StoreProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import store from "../lib/storeProvider"
+
 
 export default function Home() {
   return (

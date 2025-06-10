aa0259915a08a015432bc5b04a712123cda79525
Lsimelus <lsimelus@gmail.com>
2025-06-10 11:24:01 -0400

    point to back end

--- src/api/fetchhData.tsx
+++ src/api/fetchhData.tsx
@@ -4,7 +4,7 @@ import { createSlice, PayloadAction, createAsyncThunk,} from "@reduxjs/toolkit";
 export const fetchStateInfo = createAsyncThunk(
   "location/StateInfo",
   async (state: string,  { rejectWithValue }) => {
-    const response = await fetch("http://127.0.0.1:5000/cities/"+state);
+    const response = await fetch("https://flask-backend-inky.vercel.app/cities/"+state);
     const data = await response.json();
     return  data
   }
@@ -13,7 +13,7 @@ export const fetchStateInfo = createAsyncThunk(
 export const fetchCityInfo = createAsyncThunk(
   "location/fetchCityInfo",
   async (url: string, { rejectWithValue }) => {
-    const response = await fetch("http://127.0.0.1:5000/items/"+ url);
+    const response = await fetch("https://flask-backend-inky.vercel.app/items/"+ url);
     const data = await response.json();
     return  data;
   }
